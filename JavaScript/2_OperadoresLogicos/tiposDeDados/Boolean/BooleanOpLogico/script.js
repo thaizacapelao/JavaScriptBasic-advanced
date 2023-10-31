0066b76a989445e088f7f6dc8916b35fe0fefd5b
Thaiza Capelão <thaizacapelao@gmail.com>
2023-10-30 20:13:14 -0400

    Adicionando novas aulas

---
+++ JavaScript/2_OperadoresLogicos/tiposDeDados/Boolean/BooleanOpLogico/script.js
@@ -0,0 +1,9 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+
+console.log(3 == 3 && "Thaiza" == "Thaiza");
+console.log("Thaiza" == "Thaisa" || false);
+
+
+console.log(!(true && true));
---
+++ JavaScript/2_OperadoresLogicos/tiposDeDados/Boolean/BooleanOpTernario/script.js
@@ -0,0 +1,3 @@
+console.log( 5 > 2 ? 'É sim' : 'É não');
+console.log( false ? 5 : 4);
+console.log("Thaiza" == "Thaiza" ? "Olá Thaiza!" : "Não é a Thaiza!");

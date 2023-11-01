df1692cfe31fc74a3d6522b117794f05ee4b1e6b
Thaiza Capelão <thaizacapelao@gmail.com>
2023-11-01 17:18:03 -0400

    Adicionando estruturas de programacao

---
+++ JavaScript/4_EstruturasDeProgramacao/script.js
@@ -0,0 +1,22 @@
+let nome = 'Thaiza';
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let hera = 100;
+console.log(hera * hera);
+
+nome = 'Maria';
+
+console.log(nome);
+
+hera = 38729387;
+
+hera = 'Hera';
+
+console.log(hera);
+
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
+
+//uma possibilidade

797df280e0ba5312865b1669f37bb38dea9f0960
Thaiza Capelão <thaizacapelao@gmail.com>
2023-11-21 14:44:53 -0400

    adicionando aulas

---
+++ JavaScript/4_EstruturasDeProgramacao/1_Variaveis/script.js
@@ -0,0 +1,5 @@
+var nome = "Thaiza";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
---
+++ JavaScript/4_EstruturasDeProgramacao/2_Convencao/script.js
@@ -0,0 +1,7 @@
+let nome3 = 'teste1';
+let $nome= 'teste2';
+let _nome = 'teste3';
+let Nome = 'teste4';
+let seuNome = 'teste5';
+// let @nome = 'teste6';
+// let !nome = 'teste7';

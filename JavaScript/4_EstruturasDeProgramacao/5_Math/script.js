6b8c3fd2e0f85d63c0dcc1c797ce85e6ed84fe41
Thaiza Capelão <thaizacapelao@gmail.com>
2023-11-21 16:45:20 -0400

    adicionando aulas 54 e 55

---
+++ JavaScript/4_EstruturasDeProgramacao/5_Math/script.js
@@ -0,0 +1,15 @@
+let maior = Math.max(4, 5, 9, 21, 159);
+
+console.log(maior);
+
+let menor =  Math.min(4, 5, 9, 21, 159);
+
+console.log(menor);
+
+let arredondar = Math.round(4.56664232);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(4.56664232);
+
+console.log(arredondarParaCima);
---
+++ JavaScript/4_EstruturasDeProgramacao/6_Console/script.js
@@ -0,0 +1,7 @@
+let idade = 26;
+let nome = "Thaiza";
+
+console.log(idade);
+console.log(nome);
+
+console.log(`O meu nome é ${nome}, e tenho ${idade} anos.`);

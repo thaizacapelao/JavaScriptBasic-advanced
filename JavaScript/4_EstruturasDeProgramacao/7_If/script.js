8be8ccdf6496c342d1ac26bdcce6875173ba2883
Thaiza Capelão <thaizacapelao@gmail.com>
2023-11-29 16:32:21 -0400

    Adicionando aulas 58 e 59

---
+++ JavaScript/4_EstruturasDeProgramacao/7_If/script.js
@@ -0,0 +1,22 @@
+let idade = 19;
+
+
+if (idade == 19) {
+    console.log("A idade é 19");
+}
+
+if (idade > 25) {
+    console.log("Pode entrar");
+}
+
+let nome = "Thaiza";
+
+if (nome == "Thaiza" && idade > 10) {
+    console.log("Liberada!");
+}
+
+let passaporte = true;
+
+if (nome == "Thaiza" && idade > 30 || passaporte == true) {
+    console.log("Liberação completa");
+}

589e35c5ac188fe24aca9632a734278142033fb1
Thaiza Capelão <thaizacapelao@gmail.com>
2023-12-07 16:34:18 -0400

    Adicionando aula 67 e 68

---
+++ JavaScript/4_EstruturasDeProgramacao/15_Resumo_incr/script.js
@@ -0,0 +1,15 @@
+let numero = 5;
+
+for(let i = 0; i < 10; i++) {
+    console.log("O i é " + i);
+}
+
+while(numero < 50) {
+    console.log(numero);
+
+    numero += 10;
+}
+
+for(let i = 10; i > 0; i--) {
+    console.log("O i é " + i);
+}
---
+++ JavaScript/4_EstruturasDeProgramacao/15_Switch/script.js
@@ -0,0 +1,12 @@
+let nome = "Thaiza";
+
+switch(nome) {
+    case "Thaiza":
+        console.log("O nome é Thaiza");
+        break;
+    case "Gabriel":
+        console.log("O nome é Gabriel");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+}

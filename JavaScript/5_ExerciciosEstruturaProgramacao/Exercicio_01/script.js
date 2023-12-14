4f6fe9a7bc491b57d63dee4b85796343e3006435
Thaiza Capelão <thaizacapelao@gmail.com>
2023-12-14 14:30:21 -0400

    Adicionando exercicio 01 de estrutura de programacao

---
+++ JavaScript/5_ExerciciosEstruturaProgramacao/Exercicio_01/script.js
@@ -0,0 +1,15 @@
+const nome = "Thaiza";
+const idade = 26;
+const programadora = true;
+
+if (programadora == true) {
+    console.log("É programadora");
+};
+
+console.log("Meu nome é " + nome);
+console.log(`Minha idade é: ${idade}, ou seja 1997 - 2023 = ${2023 - 1997}`);
+console.log(`Sou Programadora? ${programadora}`);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof programadora);

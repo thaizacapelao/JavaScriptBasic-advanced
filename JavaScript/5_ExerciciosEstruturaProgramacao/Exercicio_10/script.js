fd09b3fcb78651ee96c1730a567ff8f052850524
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-11 15:27:16 -0400

    Adicionando exercicio 10 de estrutura de repetição

---
+++ JavaScript/5_ExerciciosEstruturaProgramacao/Exercicio_10/script.js
@@ -0,0 +1,12 @@
+for(let t = 0; t <= 50; t++) {
+
+    if(t % 2 == 0) {
+    console.log(`"O numero ${t} é par"`);
+    } else {  
+     console.log(`"O numero ${t} é ímpar"`);
+    }
+};
+
+/* % = Não é DIVIDIDO (Dividido é /)
+% = DIVISIVEL
+*/

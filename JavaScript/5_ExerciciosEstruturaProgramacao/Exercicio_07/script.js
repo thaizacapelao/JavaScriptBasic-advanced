7716b69ba2d7af166e4e4556d78ebdb3fe2e189d
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-11 14:18:38 -0400

    Adicionando exercicio 06 de estrutura de programacao

---
+++ JavaScript/5_ExerciciosEstruturaProgramacao/Exercicio_07/script.js
@@ -0,0 +1,16 @@
+// const idade = 18;
+// const cnh = true;
+
+// const idade = 18;
+// const cnh = false;
+
+const idade = 17;
+const cnh = false;
+
+if(idade >= 18 && cnh == true) {
+    console.log("É maior de 18 e tem Chn! Esta apto!")
+} else if (idade >= 18 && cnh == false) {
+    console.log("É maior de 18 anos, mas não tem cnh! Não está apto!")
+} else {
+    console.log("Não tem CNH nem é maior de 18 anos! Não está apto!")
+};

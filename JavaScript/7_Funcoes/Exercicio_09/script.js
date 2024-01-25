3fb179a3caf666699c329fb231096ce78bf0692c
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-25 15:39:40 -0400

    Adicionando categoria de funcoes

---
+++ JavaScript/7_Funcoes/Exercicio_09/script.js
@@ -0,0 +1,25 @@
+function imprimirNoConsole() {
+    console.log("Olá Mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log("O número é: " + num);
+}
+
+imprimirUmNumero(1);
+imprimirUmNumero(2);
+imprimirUmNumero(3);
+imprimirUmNumero(4);
+imprimirUmNumero(5);
+
+const imprimirNumeroAleatorio = function() {
+    console.log(Math.random());
+};
+
+imprimirNumeroAleatorio();
+imprimirNumeroAleatorio();
+imprimirNumeroAleatorio();
+imprimirNumeroAleatorio();
+imprimirNumeroAleatorio();

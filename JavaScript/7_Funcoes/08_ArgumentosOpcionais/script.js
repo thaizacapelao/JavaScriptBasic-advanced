7df242c19bda6f28ea6f54d3ce2448170bc78bd4
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-29 18:05:13 -0400

    Adicionando categoria de Argumentos Opcionais

---
+++ JavaScript/7_Funcoes/08_ArgumentosOpcionais/script.js
@@ -0,0 +1,23 @@
+function soma(a,b) {
+
+    if(a === undefined || b === undefined) {
+        console.log("Esta função precisa ter os dois argumentos");
+    } else {
+        return a + b;
+    }
+}
+
+console.log(soma(1));
+
+console.log(soma(1,2));
+
+function saudacao(nome, idade) {
+    if(idade === undefined) {
+        console.log("Olá " + nome);
+    } else {
+        console.log("Olá " + nome + " você tem " + idade);
+    }
+}
+
+saudacao("Thaiza");
+saudacao("Thaiza", "26");

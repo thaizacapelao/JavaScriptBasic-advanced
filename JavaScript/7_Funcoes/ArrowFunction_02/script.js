4600574f87c7a680a5b5951adc157b586e911878
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-29 17:08:55 -0400

    Adicionando categoria de Arrow Function

---
+++ JavaScript/7_Funcoes/ArrowFunction_02/script.js
@@ -0,0 +1,10 @@
+const multiplicarPorDois = (x) => {
+    return x * 2;
+}
+console.log(multiplicarPorDois(2));
+
+// Maneira mais simplicada de fazer a mesma função
+
+const multiplicarPorDois2 = (x) => x * 2;
+
+console.log(multiplicarPorDois2(4));

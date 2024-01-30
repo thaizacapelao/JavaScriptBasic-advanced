4fb59d89d08feeabbe95551d091641f6d3953df4
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-29 22:33:19 -0400

    Adicionando categoria de Closure

---
+++ JavaScript/7_Funcoes/09_Clousure/script.js
@@ -0,0 +1,29 @@
+function lembrarSoma(x) {
+    return function(y) {
+        return x + y;
+    }
+}
+
+let soma1 = lembrarSoma(2);
+
+console.log(soma1(5));
+
+let soma2 = lembrarSoma(5);
+
+console.log(soma2(7));
+
+function contador(i) {
+    let cont = i;
+    let somarContador = function() {
+        console.log(cont);
+        cont++; 
+    };
+    return somarContador;
+}
+
+let meuContador = contador(5);
+meuContador();
+meuContador();
+meuContador();
+meuContador();
+meuContador();

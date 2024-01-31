dd478d10ce6ae8ff9b26550c86e06546c95a748c
Thaiza Capelão <thaizacapelao@gmail.com>
2024-01-31 18:02:57 -0400

    Adicionando Exercicio 03 de Funções - parametros de soma

---
+++ JavaScript/8_ExerciciosDeFuncoes/Exercicio_03/script.js
@@ -0,0 +1,8 @@
+function somarDoisParametros(x, y) {
+    return x + y;
+};
+
+console.log(somarDoisParametros(10, 2));
+console.log(somarDoisParametros(40, 2));
+console.log(somarDoisParametros(560, 756));
+console.log(somarDoisParametros(3453, 64));

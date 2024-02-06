adbaddc9bd2763aeaaffb96d6e2fe5de2475ea2c
Thaiza Capelão <thaizacapelao@gmail.com>
2024-02-06 15:21:24 -0400

    Adicionando Exercicio 06  de Funções - Tipo de Dado

---
+++ JavaScript/8_ExerciciosDeFuncoes/Exercicio_06/script.js
@@ -0,0 +1,16 @@
+function identificarTipodeDeDado(dado) {
+    if(typeof dado === 'string') {
+        console.log("Este dado é uma string");
+    } else if(typeof dado === 'number'){
+        console.log("Este dado é um number");
+    } else if(typeof dado === 'boolean'){
+        console.log("Este dado é um boolean");
+    };
+};
+
+identificarTipodeDeDado(4);
+identificarTipodeDeDado('Thaiza');
+identificarTipodeDeDado(true);
+identificarTipodeDeDado(false);
+identificarTipodeDeDado(19);
+identificarTipodeDeDado('Maria');
---
+++ JavaScript/8_ExerciciosDeFuncoes/Exercicio_08/script.js
@@ -0,0 +1,13 @@
+function recebeIdadeAutoescola(idade) {
+    if(idade >= 18) {
+        console.log("Pode se matricular na auto escola");
+    } else{
+        console.log("Não pode se matricular na auto escola");
+    }
+};
+
+recebeIdadeAutoescola(4);
+recebeIdadeAutoescola(10);
+recebeIdadeAutoescola(29);
+recebeIdadeAutoescola(18);
+recebeIdadeAutoescola(19);

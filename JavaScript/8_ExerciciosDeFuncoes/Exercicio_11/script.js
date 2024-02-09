0324f941d89367cf1c254abee46c9bf0de16b71f
Thaiza Capelão <thaizacapelao@gmail.com>
2024-02-08 21:36:18 -0400

    Adicionando Exercicio 11 de Funções - Somando 3 itens

---
+++ JavaScript/8_ExerciciosDeFuncoes/Exercicio_11/script.js
@@ -0,0 +1,15 @@
+function soma(x, y, z) {
+    return x + y + z
+}
+
+var resultado = soma(5, 10, 15)
+
+/*
+Crie uma função chamada soma que aceita três números como argumentos;
+
+Retorne a soma dos três números;
+
+Crie uma variável chamada resultado;
+
+Ela deve conter o retorno da função, passe os números 5, 10 e 15 como argumento;
+*/

5fd3b6e58bfde2c4c2b9f4afb8cb056abcd1ad7f
Thaiza Capelão <thaizacapelao@gmail.com>
2024-02-16 16:56:17 -0400

    Adicionando Exercicio 13 de Funções - Calculo de Média

--- JavaScript/8_ExerciciosDeFuncoes/Exercicio_13/script.js
+++ JavaScript/8_ExerciciosDeFuncoes/Exercicio_13/script.js
@@ -3,3 +3,21 @@ function calculateAverage(numbers) {
       numbers.reduce((sum, num) => sum + num, 0) /
       numbers.length;
   }
+
+  function calcularMedia(numbers) {
+    var soma;
+    numbers.forEach((num) => soma = soma + num);
+
+    return soma === 0 ? 0 : soma / numbers.length;
+  }
+
+  function calcularMediaFor(numbers) {
+    var soma;
+
+    for(let i = 0; i < numbers.length; i = i ) {
+        let num = numbers[i];
+        soma = soma + num;
+    }
+
+    return soma === 0 ? 0 : soma / numbers.length;
+  }

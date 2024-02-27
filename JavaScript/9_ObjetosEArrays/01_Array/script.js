20f8a5be81104aadfcd3c3a1417e899b653e558f
Thaiza Capelão <thaizacapelao@gmail.com>
2024-02-27 16:58:01 -0400

    Adicionando aula 01 e 02 Arrays e Objetos

---
+++ JavaScript/9_ObjetosEArrays/01_Array/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['Thaiza', 'Gabriel', 'Lina'];
+let bool = [true, false, true];
+
+let misturado = [26, 'Thaiza', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
---
+++ JavaScript/9_ObjetosEArrays/02_SobreArrays/script.js
@@ -0,0 +1,12 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['Thaiza', 'Gabriel', 'Lina'];
+let bool = [true, false, true];
+
+let misturado = [26, 'Thaiza', true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1]); // acessar o ultimo elemento do array
+// mesmo que não saiba qual seja
+

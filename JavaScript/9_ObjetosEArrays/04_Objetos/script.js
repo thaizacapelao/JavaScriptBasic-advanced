30a8f7cfd2e9682d0fb6a66fcd387171275daa1d
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-11 17:25:16 -0400

    Adicionando aula 05 Objetos

---
+++ JavaScript/9_ObjetosEArrays/04_Objetos/script.js
@@ -0,0 +1,15 @@
+let cachorro = {
+    nome: 'Lina',
+    patas: 4,
+    latir: function() {
+        console.log("au au");
+    }
+};
+
+console.log(cachorro.nome);
+console.log(cachorro.patas);
+cachorro.latir();
+
+
+// [] = Array
+// {} = Objeto

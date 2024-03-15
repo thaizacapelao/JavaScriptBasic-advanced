a8621c37f1afa59200012aeb30c82066937c9c8b
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-15 16:13:40 -0400

    Adicionando aula 13 IndexOf e LastIndexOf

---
+++ JavaScript/9_ObjetosEArrays/13_IndexOf_E_LastIndexOf/script.js
@@ -0,0 +1,5 @@
+let nums = [5,6,2,4,9,6,2];
+
+console.log(nums.indexOf(2)); // procura onde no array o 2 aparece pela primeira vez (em que posição)
+
+console.log(nums.lastIndexOf(2)); // procura a ultima vez que o 2 aparece no array

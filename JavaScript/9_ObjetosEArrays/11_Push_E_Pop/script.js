02e536bcbd14c9a7c1414da1ac8530e2ec47c335
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-15 15:17:19 -0400

    Adicionando aula 11 Push e Pop

---
+++ JavaScript/9_ObjetosEArrays/11_Push_E_Pop/script.js
@@ -0,0 +1,13 @@
+// Push = inserir um elemento no array (NO FINAL)
+// Pop = retirar um elemento no array (NO FINAL)
+
+let nomes = ['Thaiza', 'da', 'Silva', 'Capelão'];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push('Ferreira');
+
+console.log(nomes);

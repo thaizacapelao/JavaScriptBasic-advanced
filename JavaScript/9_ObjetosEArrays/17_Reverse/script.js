189bc758d0e90c174bb80ad0d909f52846a17d87
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-19 16:51:38 -0400

    Adicionando aula 17 Reverse

---
+++ JavaScript/9_ObjetosEArrays/17_Reverse/script.js
@@ -0,0 +1,9 @@
+// serve para reverter um array 
+
+let carros = ["BMW", "Mercedes", "Jeep", "Audi","VW", "Fiat"];
+
+console.log(carros.reverse());
+
+let numeros = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+console.log(numeros.reverse());

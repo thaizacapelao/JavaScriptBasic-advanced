231e25a1afcc33e73853e7c85e34eee9093139ae
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-20 17:28:44 -0400

    Adicionando aula 19 padStart

--- JavaScript/9_ObjetosEArrays/19_padStart/script.js
+++ JavaScript/9_ObjetosEArrays/19_padStart/script.js
@@ -1,11 +1,9 @@
 let sku = "34";
-
 console.log(sku.padStart(6, "0"));
 
 // Bastante utilizado em lojas;
 
 let sku2 = "7348";
-
 console.log(sku2.padStart(6, "0"));
 
 // no "6" definimos a quantidade limite de caracteres

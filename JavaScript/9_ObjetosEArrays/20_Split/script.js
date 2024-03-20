c3b629e74a47cecb7d447facc376f9c60f1dc0fa
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-20 17:44:58 -0400

    Adicionando aula 20 Split

---
+++ JavaScript/9_ObjetosEArrays/20_Split/script.js
@@ -0,0 +1,13 @@
+let frase = "Testando o Método Split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let compras = "Banana;Uva;maça;pêra;jabuticaba";
+
+let separandoCompras = compras.split(';');
+
+console.log(separandoCompras);

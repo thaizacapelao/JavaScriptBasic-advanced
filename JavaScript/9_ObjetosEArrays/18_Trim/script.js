bce18faa5e0be14e40114ef7919d09a1b7415ed8
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-20 17:28:18 -0400

    Adicionando aula 18 Trim

---
+++ JavaScript/9_ObjetosEArrays/18_Trim/script.js
@@ -0,0 +1,6 @@
+let nome = "        \n Thaiza  ";
+
+let nomeCorrigido = nome.trim();
+
+console.log(nome);
+console.log(nomeCorrigido);
---
+++ JavaScript/9_ObjetosEArrays/19_padStart/script.js
@@ -0,0 +1,12 @@
+let sku = "34";
+
+console.log(sku.padStart(6, "0"));
+
+// Bastante utilizado em lojas;
+
+let sku2 = "7348";
+
+console.log(sku2.padStart(6, "0"));
+
+// no "6" definimos a quantidade limite de caracteres
+// E no "0" definimos que o que faltar será preenchido por zeros padronizando uma string

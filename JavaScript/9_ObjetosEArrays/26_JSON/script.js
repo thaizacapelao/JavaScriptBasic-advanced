38846aef2ea3747771345bbbad25628579c57ed7
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-21 14:48:30 -0400

    Adicionando aula 27 JSON

--- JavaScript/9_ObjetosEArrays/26_JSON/script.js
+++ JavaScript/9_ObjetosEArrays/26_JSON/script.js
@@ -1,9 +1,11 @@
 let pessoa = {
-    "nome":"Thaiza",
-    "idade":"26",
-    "peso":"58"
+    "nome": "Thaiza",
+    "idade": 26,
+    "peso": 58
 }
 
 console.log(pessoa.nome);
 console.log(pessoa.idade);
 console.log(pessoa.peso);
+
+//não aceita comentarios

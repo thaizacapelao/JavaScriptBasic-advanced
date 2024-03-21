f2aa2e39cf1ff3fbd1d54af098653ac4b37c5114
Thaiza Capelão <thaizacapelao@gmail.com>
2024-03-21 12:43:38 -0400

    Adicionando aula 24 Destructuring

---
+++ JavaScript/9_ObjetosEArrays/24_Destructuring/script.js
@@ -0,0 +1,13 @@
+let obj = {
+    rodas: 4,
+    portas: 4,
+    tetoSolar: true,
+    motor: '2.0'
+}
+
+const {rodas: vRodas, portas: vPortas, tetoSolar: vTetoSolar, motor: vMotor} = obj;
+
+console.log(vRodas);
+console.log(vPortas);
+console.log(vTetoSolar);
+console.log(vMotor);
